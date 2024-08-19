1a295cd8306ca0357303fb1d6852f3ee9ea7bcc2
Mohd Asad Naseem <87413907+AsadGuru@users.noreply.github.com>
2024-08-20 00:53:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+
+describe("example to-do app", () => {
+  beforeEach(() => {
+    cy.visit(baseUrl);
+  });
+  it("Checking tags", () => {
+    cy.get('h1').should('have.length', 1)
+    cy.get('div').should('have.length', 5) // Updated this line
+    cy.get('p').should('have.length', 4)
+  });
+  it('testing parent div', () => {
+    const properties = ['padding', 'background-color']
+    properties.forEach(property => {
+      cy.get('div')
+      .first()
+      .should('have.css', property)
+    })
+  })
+  it('testing child divs', () => {
+    const properties = ['padding', 'background-color', 'border']
+    properties.forEach(property => {
+      cy.get('div')
+      .eq(1)
+      .should('have.css', property)
+      cy.get('div')
+      .eq(2)
+      .should('have.css', property)
+    })
+  }
+});
